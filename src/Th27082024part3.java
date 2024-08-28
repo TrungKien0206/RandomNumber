1a3be651ab028b3a1020c85013920ebd06f08c1c
TonyLe <149919663+TrungKien0206@users.noreply.github.com>
2024-08-28 22:29:38 +0700

    Add files via upload

---
+++ src/Th27082024part3.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Th27082024part3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String check;
+        do {
+            System.err.println("Tiep tuc chuong trinh (yes or no) ? ");
+            check = scanner.next();
+            int n = random.nextInt(100) + 1;
+            if (n % 2 == 0) {
+                System.out.println(n + " la so chan");
+            } else {
+                System.out.println(n + " la so le");
+            }
+
+        } while (check.equals("yes"));
+    }
+}
